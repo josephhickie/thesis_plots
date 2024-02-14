8ee6a5dad8ae4c0d0e9a5224a83338e4507096b9
jdh <joe.hickie@hotmail.co.uk>
2024-02-14 12:08:54 +0000

    fixed bug

--- final_chapter/figure1.py
+++ final_chapter/figure1.py
@@ -53,61 +53,62 @@ def process_2d(dictionary, data_name, x='dac10', y='dac13'):
     return data, lims
 
 
-counts = get_id(1811)
-x = 'dac10'
-y = 'dac13'
-lims = [counts.get(x).get('data').min(), counts.get(x).get('data').max(), counts.get(y).get('data').min(), counts.get(y).get('data').max()]
-counts_data = counts.get('num_states').get('data').T
+if __name__ == '__main__':
+    counts = get_id(1811)
+    x = 'dac10'
+    y = 'dac13'
+    lims = [counts.get(x).get('data').min(), counts.get(x).get('data').max(), counts.get(y).get('data').min(), counts.get(y).get('data').max()]
+    counts_data = counts.get('num_states').get('data').T
 
 
 
-# plt.figure()
-# plt.imshow(counts.get('num_states').get('data').T, origin='lower', extent=lims)
-# plt.xlabel('DAC 10')
-# plt.ylabel('DAC 13')
-# plt.show()
+    # plt.figure()
+    # plt.imshow(counts.get('num_states').get('data').T, origin='lower', extent=lims)
+    # plt.xlabel('DAC 10')
+    # plt.ylabel('DAC 13')
+    # plt.show()
 
 
-peaks = get_id(1797)
-#
-# plt.figure()
-# plt.plot(peaks.get('x').get('data'), peaks.get('plot').get('data'))
-# plt.show()
+    peaks = get_id(1797)
+    #
+    # plt.figure()
+    # plt.plot(peaks.get('x').get('data'), peaks.get('plot').get('data'))
+    # plt.show()
 
 
 
-stability = get_id(2472)
+    stability = get_id(2472)
 
-stab_data, stab_lims = process_2d(stability, 'C', x='dac9', y='comp12')
-stab_lims[3] = stab_lims[3] + 40
+    stab_data, stab_lims = process_2d(stability, 'C', x='dac9', y='comp12')
+    stab_lims[3] = stab_lims[3] + 40
 
-# plt.figure()
-# plt.imshow(stab_data, origin='lower', extent=stab_lims)
-# plt.xlabel('DAC 10')
-# plt.ylabel('DAC 13')
-# plt.show()
-
-
-fig, axs = plt.subplots(2, 2, gridspec_kw={'height_ratios': [3, 1]})
-
-p = axs[0, 0].imshow(stab_data, origin='lower', extent=stab_lims, cmap='hot')
-axs[0, 0].set_xlabel('DAC 10 / mV')
-axs[0, 0].set_ylabel('DAC 13 / mV')
-axs[0, 1].set_xlabel('DAC 10 / mV')
-axs[0, 1].set_ylabel('DAC 13 / mV')
-
-axs[1, 0].hist(stab_data.flatten(), bins=80, color='black')
-axs[1, 0].set_ylabel('Counts')
-axs[1, 0].set_xlabel('$V_\mathrm{rf}$')
-
-axs[1, 1].plot(peaks.get('x').get('data'), peaks.get('plot').get('data') / np.max(peaks.get('plot').get('data')), color='black')
-axs[1, 1].set_xlabel('Pixel')
-axs[1, 1].set_ylabel('$\\nabla V_\mathrm{rf} \, (a. u.)$')
+    # plt.figure()
+    # plt.imshow(stab_data, origin='lower', extent=stab_lims)
+    # plt.xlabel('DAC 10')
+    # plt.ylabel('DAC 13')
+    # plt.show()
 
 
-plot = axs[0, 1].imshow(counts_data, origin='lower', extent=lims)
-plt.colorbar(plot, ax=axs[0, 1], orientation='horizontal', location='top')
-plt.colorbar(p, ax=axs[0, 0], orientation='horizontal', location='top')
-fig.tight_layout()
-plt.savefig('./counts.svg')
-plt.show()
+    fig, axs = plt.subplots(2, 2, gridspec_kw={'height_ratios': [3, 1]})
+
+    p = axs[0, 0].imshow(stab_data, origin='lower', extent=stab_lims, cmap='hot')
+    axs[0, 0].set_xlabel('DAC 10 / mV')
+    axs[0, 0].set_ylabel('DAC 13 / mV')
+    axs[0, 1].set_xlabel('DAC 10 / mV')
+    axs[0, 1].set_ylabel('DAC 13 / mV')
+
+    axs[1, 0].hist(stab_data.flatten(), bins=80, color='black')
+    axs[1, 0].set_ylabel('Counts')
+    axs[1, 0].set_xlabel('$V_\mathrm{rf}$')
+
+    axs[1, 1].plot(peaks.get('x').get('data'), peaks.get('plot').get('data') / np.max(peaks.get('plot').get('data')), color='black')
+    axs[1, 1].set_xlabel('Pixel')
+    axs[1, 1].set_ylabel('$\\nabla V_\mathrm{rf} \, (a. u.)$')
+
+
+    plot = axs[0, 1].imshow(counts_data, origin='lower', extent=lims)
+    plt.colorbar(plot, ax=axs[0, 1], orientation='horizontal', location='top')
+    plt.colorbar(p, ax=axs[0, 0], orientation='horizontal', location='top')
+    fig.tight_layout()
+    plt.savefig('./counts.svg')
+    plt.show()
